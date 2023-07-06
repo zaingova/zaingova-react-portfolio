8b0801afa94382e0470ce4472961ad70941d11dd
zaingova <zain.gova.school@gmail.com>
2023-07-06 09:50:12 -0400

    made some styling changed to the columns and the submit button on the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,19 +4,18 @@ import '../../assets/css/input.css';
 
 export default function Contact() {
   return (
-    <div className='col-7 m-3 p-4'>
+    <div className='col-8 m-3 p-4'>
       <h1 className='title m-0 text-left'>Contact Me!</h1>
 
       <div className='col-12'>
         <p className='p-4 bg-light shadow'>
-          Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-          Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-          dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-          sodales volutpat et, hendrerit ut dolor. Donec nec ipsum
-          in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-          bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-          est ligula suscipit velit, sed bibendum turpis dui in sapien.</p>
+          If you'd like to contact me regarding any matter you can do so using the form below! Please be wary of what you send for testing/grading, as the form is linked to my <b>personal email!</b>
+          <hr></hr>
+          Thank you for your consideration and I look forward to hearing from you!
+          </p>
       </div>
+
+      <hr></hr>
 
       <div className='col-12 card bg-light px-4 py-5 shadow'>
         <form>
@@ -28,13 +27,13 @@ export default function Contact() {
               <input type="text" class="form-control" id="first_name" placeholder="First name"></input>
             </div>
             <div className='form-group col'>
-              <input type="text" class="form-control" id="first_name" placeholder="First name"></input>
+              <input type="text" class="form-control" id="first_name" placeholder="Last name"></input>
             </div>
           </div>
           <div>
             <textarea className='col-12 p-2' rows={5} placeholder='Your message...'></textarea>
           </div>
-          <button className='btn-lg btn btn-dark w-25'>Submit</button>
+          <button className='btn-lg btn btn-contact w-25'>Submit</button>
         </form>
       </div>
 
